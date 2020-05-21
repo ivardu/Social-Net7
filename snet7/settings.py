63eea8c89a7a1d70fc02138d6d84fb1fd4dbb43b
Ravi Kumar <ivardu19@gmail.com>
2020-05-21 16:55:47 +0100

    settings changes like removing db

--- snet7/settings.py
+++ snet7/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'coverage',
+    # 'coverage',
     'crispy_forms',
 ]
 
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'snet7.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
